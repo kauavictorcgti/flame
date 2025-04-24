5e89a49a73a989c89c7e0c97d324d01fcbde1553
kaua <kaua.oliveira@ifba.edu.br>
2025-04-24 09:00:55 -0300

    fixed search

--- client/src/utility/regexSearch.ts
+++ client/src/utility/regexSearch.ts
@@ -3,12 +3,7 @@ import { escapeRegex } from "./escapeRegex";
 
 export const regexSearch = (search: string, text: string) => {
 
-    const searchTerms = search.trim().split(/%20/);
-
-    console.log(search)
-    console.log(searchTerms)
-    console.log(text)
-
-    return searchTerms.every(term => new RegExp(escapeRegex(term), 'i').test(text))
+    const query = search.trim().replace(/%20/, ' ');
+    return new RegExp(escapeRegex(query), 'i').test(text)
 
 }
